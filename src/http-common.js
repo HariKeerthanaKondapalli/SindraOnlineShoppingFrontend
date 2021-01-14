9ee2199e0cd00f4b5d1378107ef814baeec53622
HariKeerthanaKondapalli <55994028+HariKeerthanaKondapalli@users.noreply.github.com>
2021-01-14 10:29:40 +0530

    Update http-common.js

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://sindra-onlineshopping.herokuapp.com/",
+  baseURL: "https://sindra-onlineshopping.herokuapp.com/",
   headers: {
     "Content-type": "application/json"
   }
